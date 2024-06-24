2ab25f8cd1a3e96207de285510e082cb4948ee17
MohamedAmineHammi <eminehammi@gmail.com>
2024-06-24 12:04:04 +0100

    Secure Spring Rest API in Spring Boot 3 | Spring Boot 3 Security Configuration

---
+++ src/main/java/com/dailycodework/ilibrary/security/LibrarySecurityConfig.java
@@ -0,0 +1,36 @@
+package com.dailycodework.ilibrary.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class LibrarySecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] SECURED_URLs = {"/books/**"};
+    private static final String[] UN_SECURED_URLs = {
+            "/books/all",
+            "books/book/{id}",
+            "/users/**"
+    };
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers(UN_SECURED_URLs).permitAll().and()
+                .authorizeHttpRequests().requestMatchers(SECURED_URLs)
+                .hasAnyAuthority("ADMIN").anyRequest()
+                .authenticated().and().httpBasic().and().build();
+    }
+}
